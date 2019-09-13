f8a6e005438e1a0bd021b4c11e42d1610a1428fc
Dante12129 <Blahduke31398ap>
2019-09-13 00:23:53 -0400

    Create HumanView

---
+++ include/HumanView.hpp
@@ -0,0 +1,15 @@
+//
+// Created by dan9c on 9/13/2019.
+//
+
+#ifndef CSCI437_HUMANVIEW_HPP
+#define CSCI437_HUMANVIEW_HPP
+
+
+class HumanView
+{
+
+};
+
+
+#endif //CSCI437_HUMANVIEW_HPP
---
+++ src/HumanView.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dan9c on 9/13/2019.
+//
+
+#include "HumanView.hpp"
